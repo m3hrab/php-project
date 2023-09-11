684511dd4f9f8594d46a0aa4de50f27087e4fe96
m3hrab <mehrab.cse16@gmail.com>
2023-09-12 04:14:21 +0600

    update

--- doctors.php
+++ doctors.php
@@ -5,27 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor List</title>
     <style>
+        /* Style for the container holding each doctor's information */
         .doctor-container {
             display: flex;
             justify-content: space-between;
             align-items: center;
-            margin:20px 100px;
+            margin-bottom: 20px;
             padding: 10px;
             border: 1px solid #ccc;
         }
 
+        /* Style for the doctor's image */
         .doctor-image {
-            max-width: 300px;
-            max-height: 300px;
+            max-width: 100px;
+            max-height: 100px;
             margin-right: 10px;
         }
 
+        /* Style for the doctor's information (Name, Specialist, Clinic Address) */
         .doctor-info {
             flex-grow: 1;
             margin-right: 10px;
         }
 
+        /* Style for the button */
         .appointment-button {
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            padding: 10px 15px;
+            cursor: pointer;
+        }
+
+        /* Style for the appointment modal */
+        .appointment-modal {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+            z-index: 1;
+        }
+
+        /* Style for the appointment modal content */
+        .appointment-modal-content {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+            text-align: center;
+        }
+
+        /* Style for the available time slots */
+        .available-times {
+            margin: 20px 0;
+        }
+
+        /* Style for the "Confirm Appointment" button */
+        .confirm-appointment-button {
             background-color: #007bff;
             color: #fff;
             border: none;
@@ -35,6 +76,7 @@
     </style>
 </head>
 <body>
+    <!-- Your PHP and existing HTML code here -->
     <?php
     include 'dbcon.php';
 
@@ -52,7 +94,7 @@
             echo "<strong>Specialist:</strong> " . $row['specialist'] . "<br>";
             echo "<strong>Clinic Address:</strong> " . $row['clinic_address'] . "<br>";
             echo "</div>";
-            echo "<button class='appointment-button'>Make an Appointment</button>";
+            echo "<button class='appointment-button' onclick='openModal()'>Make an Appointment</button>";
             echo "</div>";
         }
     } else {
@@ -61,5 +103,46 @@
 
     mysqli_close($con);
     ?>
+
+    <!-- Appointment Modal -->
+    <div id="appointmentModal" class="appointment-modal">
+        <div class="appointment-modal-content">
+            <h2>Set Appointment</h2>
+            <div class="available-times">
+                <p>Available Time Slots:</p>
+                <!-- You can populate the available time slots dynamically here -->
+                <select id="timeSlots">
+                    <option value="9:00 AM">9:00 AM</option>
+                    <option value="10:00 AM">10:00 AM</option>
+                    <option value="11:00 AM">11:00 AM</option>
+                    <!-- Add more options as needed -->
+                </select>
+            </div>
+            <button class="confirm-appointment-button" onclick="confirmAppointment()">Confirm Appointment</button>
+            <button onclick="closeModal()">Close</button>
+        </div>
+    </div>
+
+    <script>
+        // JavaScript functions to handle the appointment modal
+
+        // Function to open the appointment modal
+        function openModal() {
+            document.getElementById("appointmentModal").style.display = "block";
+        }
+
+        // Function to close the appointment modal
+        function closeModal() {
+            document.getElementById("appointmentModal").style.display = "none";
+        }
+
+        // Function to handle the "Confirm Appointment" button
+        function confirmAppointment() {
+            // Add your appointment confirmation logic here
+            // You can send the selected time slot to the server, update the database, etc.
+            alert("Appointment confirmed!");
+            closeModal();
+        }
+    </script>
 </body>
 </html>
